6c29158466244ef372d6f28fecabc1fb8935875a
Uranium2 <tavernier.antoine@hotmail.fr>
2020-06-29 23:24:46 +0200

    [UP] upload 2015 to 2019 cadastre to RDS

--- main.py
+++ main.py
@@ -21,26 +21,29 @@ conn = pymysql.connect(
   host= keys[2],
   user=keys[3],
   password=keys[4],
-  port=keys[5])
+  port=int(keys[5]))
 
 cursor = conn.cursor()
 
 currentDirectory = os.path.dirname(os.path.realpath(__file__))
 now = datetime.now()
 
-url_cadastre = "https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/{}/departements/".format(now.year - 1)
-print(url_cadastre)
+def get_url(year):
+    url_cadastre = "https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/{}/departements/".format(year)
+    print(url_cadastre)
+    return url_cadastre
 
-def get_date_cadastre():
-    response = requests.get(url_cadastre, headers=headers)
+def get_date_cadastre(year):
+    response = requests.get(get_url(year), headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
     res = soup.find_all('a', href=True)
     line_date = str(res[2].next_sibling)
     date = line_date.split()[0]
+    print("get_date_cadastre: {}".format(date))
     return date
 
-def check_cadastre_update():
-    file_path = os.path.join(currentDirectory, "last_date_import.txt")
+def check_cadastre_update(year):
+    file_path = os.path.join(currentDirectory, "{}.txt".format(year))
     try:
         text_file = open(file_path)
     except :
@@ -49,9 +52,10 @@ def check_cadastre_update():
 
     text_file = open(file_path, "r")
     content = text_file.read()
-    date = get_date_cadastre()
+    date = get_date_cadastre(year)
     text_file.close()
-    if content != date:
+    if date != content:
+        print(date, content)
         text_file = open(file_path, "w")
         text_file.write(date)
         text_file.close()
@@ -59,11 +63,12 @@ def check_cadastre_update():
     else:
         print("No need to update")
         return False
-    
-if check_cadastre_update():
+
+def main_process():
+    print("year: {}".format(year))
     print("DL file")
     filename = "75.csv.gz"
-    r = requests.get(url_cadastre + filename, allow_redirects=True)
+    r = requests.get(get_url(year) + filename, allow_redirects=True)
 
     local_csv = os.path.join(currentDirectory, filename)
     open(local_csv, 'wb').write(r.content)
@@ -72,7 +77,7 @@ if check_cadastre_update():
         df = pd.read_csv(f)
         
         # Pre traitement
-        df = df[["date_mutation", "valeur_fonciere", "code_type_local", "type_local", "surface_reelle_bati", "nombre_pieces_principales", "surface_terrain", "longitude", "latitude"]]
+        df = df[["date_mutation", "code_postal", "valeur_fonciere", "code_type_local", "surface_reelle_bati", "nombre_pieces_principales", "surface_terrain", "longitude", "latitude"]]
         df = df.fillna(0)
         df = df.drop_duplicates()
 
@@ -84,3 +89,9 @@ if check_cadastre_update():
             if i % 100 == 0:
                 print("Running... at line " + str(i))
             conn.commit()
+
+
+for year in range(2015, datetime.now().year): 
+    print(year)
+    if check_cadastre_update(year):
+        main_process()        
